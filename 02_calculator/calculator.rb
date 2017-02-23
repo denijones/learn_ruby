962aeb7b71f21d5ddc2222598d4c5856c173d111
Denisha Jones <denijones@ymail.com>
2017-02-23 17:51:37 -0600

    Create and complete calculator test

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,42 @@
 #write your code here
+def add(x,y)
+
+	return x + y
+
+end
+
+def subtract(x,y)
+	return x-y
+end
+
+def sum(x)
+	sum = 0
+	x.each {|y| sum += y}
+	return sum
+end
+
+def multiply(x,y,*z)
+	sum = 0
+	sum = x * y
+	z.each {|num| sum *= num}
+	return sum
+end
+
+def power(x,y)
+	return x**y
+end
+
+def factorial(x)
+	sum = 1
+	if x > 0
+		for i in (x).downto(1)
+			sum *= i
+		end
+	end
+
+
+	return sum
+end
+
+
+	
