e98f686d16ea0d8ae12a5909822f96b17b5cad31
Denisha Jones <denijones@ymail.com>
2017-03-03 01:35:28 -0600

    Crate class for adding book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,31 @@
 class Book
-# write your code here
+    #get and set title
+    attr_accessor :title
+
+    #structure the titles
+    def title=(new_title)
+        #split title to capitalize each word
+        title = new_title.split(" ")
+
+        #hold conjunctions
+        conjunctions = ["and", "in", "the", "an", "of", "a"]
+
+        #always capitalize first word regardless
+        title[0].capitalize!
+
+        #for every other word, capitalize unless it is a conjunction or number
+        title[1..-1].collect! do |i|
+
+          unless (conjunctions.include? i) or (i =~ /\d/)
+            i.capitalize!
+          else
+            i
+          end
+
+
+        end
+
+        @title = title.join(" ")
+
+    end
 end
